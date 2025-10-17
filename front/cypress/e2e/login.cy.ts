c19c52cdd7dd1dcd0a662c074e7faef4868ff82e
Cécile UMECKER <74008168+cecile-umecker@users.noreply.github.com>
2025-10-17 10:30:48 +0200

    add e2e tests for login

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -24,4 +24,52 @@ describe('Login spec', () => {
 
     cy.url().should('include', '/sessions')
   })
-});
+
+  it('Login failed', () => {
+    cy.visit('/login')
+
+    cy.intercept('POST', '/api/auth/login', {
+      statusCode: 401,
+      body: {
+        message: 'Bad credentials'
+      },
+    })
+
+    cy.get('input[formControlName=email]').type("yoga@studio.com")
+    cy.get('input[formControlName=password]').type(`${"wrongPassword"}{enter}{enter}`)
+
+    cy.url().should('include', '/login')
+  })
+
+  it('Login failed because of empty fields', () => {
+    cy.visit('/login')
+
+    cy.intercept('POST', '/api/auth/login', {
+      statusCode: 400,
+      body: {
+        message: 'Bad Request'
+      },
+    })
+
+    cy.get('input[formControlName=email]').type("{selectall}{backspace}")
+    cy.get('input[formControlName=password]').type("{selectall}{backspace}{enter}")
+
+    cy.url().should('include', '/login')
+  })
+
+  it('Login failed because of invalid email format', () => {
+    cy.visit('/login')
+
+    cy.intercept('POST', '/api/auth/login', {
+      statusCode: 401,
+      body: {
+        message: 'Bad credentials'
+      },
+    })
+
+    cy.get('input[formControlName=email]').type("invalidEmailFormat")
+    cy.get('input[formControlName=password]').type("test!1234{enter}")
+
+    cy.url().should('include', '/login')
+  })
+})
